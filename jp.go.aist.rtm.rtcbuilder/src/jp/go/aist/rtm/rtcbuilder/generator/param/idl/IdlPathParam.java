2f776569458eb2933cda3dc43e1cfcf97ace7792
Sakamoto Takeshi <sakamoto@globalassist.co.jp>
2020-06-15 10:57:48 +0900

    Modified IDL file display on DataPort tab(1.2).#215

--- jp.go.aist.rtm.rtcbuilder/src/jp/go/aist/rtm/rtcbuilder/generator/param/idl/IdlPathParam.java
+++ jp.go.aist.rtm.rtcbuilder/src/jp/go/aist/rtm/rtcbuilder/generator/param/idl/IdlPathParam.java
@@ -5,22 +5,34 @@ import java.io.Serializable;
 public class IdlPathParam implements Serializable {
 
 	private static final long serialVersionUID = 2531043621817335163L;
-	
+
 	private String path;
+	private String dispPath;
 	private boolean isDefault;
-	
+
 	public IdlPathParam() {
 	}
 
 	public IdlPathParam(String path, boolean isDefault) {
 		this.path = path;
+		this.dispPath = "";
 		this.isDefault = isDefault;
 	}
-	
+
+	public IdlPathParam(String path, String dispPath, boolean isDefault) {
+		this.path = path;
+		this.dispPath = dispPath;
+		this.isDefault = isDefault;
+	}
+
 	public String getPath() {
 		return path;
 	}
-	
+
+	public String getDispPath() {
+		return dispPath;
+	}
+
 	public boolean isDefault() {
 		return isDefault;
 	}
